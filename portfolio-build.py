b530c54d20d7670abc8629b3a2aaee1708135ff9
unknown <rahmansaqib17@gmail.com>
2020-06-29 01:17:23 +0530

    function to upload data to s3

--- portfolio-build.py
+++ portfolio-build.py
@@ -1,5 +1,7 @@
 import json
+import io
 import boto3
+from botocore.client import Config
 try:
     from StringIO import StringIO
 except ImportError:
@@ -8,17 +10,23 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     s3 = boto3.resource('s3')
-    
-    portfolio_bucket = s3.Bucket('portfolio.sr.net')
-    build_bucket = s3.Bucket('portfoliobuild.sr.net')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_filebobj('portfoliobuild.zip', portfolio_zip)
-    
+        
+    portfolio_bucket = s3.Bucket('portfolio.sr')
+    build_bucket = s3.Bucket('portfoliobuild.sr')
+        
+    portfolio_zip = StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
     with zipfile.ZipFile(portfolio_zip) as myzip:
         for nm in myzip.namelist():
             obj = myzip.open(nm)
             portfolio_bucket.upload_fileobj(obj, nm,
-             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
