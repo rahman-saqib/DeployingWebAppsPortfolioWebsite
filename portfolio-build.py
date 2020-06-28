5ed81971b801f025d1b4392ef7fd81def037fdde
unknown <rahmansaqib17@gmail.com>
2020-06-29 01:35:01 +0530

    function to upload data to s3

--- portfolio-build.py
+++ portfolio-build.py
@@ -10,23 +10,31 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:611462841437:deployPortfolioTopic')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        s3 = boto3.resource('s3')
         
-    portfolio_bucket = s3.Bucket('portfolio.sr')
-    build_bucket = s3.Bucket('portfoliobuild.sr')
+            
+        portfolio_bucket = s3.Bucket('portfolio.sr')
+        build_bucket = s3.Bucket('portfoliobuild.sr')
+            
+        portfolio_zip = StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+            
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         
-    portfolio_zip = StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        topic.publish(Subject="Portfolio Deployed", Message = "Portfolio deployed Successfully")
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+    except:
+        topic.publish(Subject="Portfolio Not Deployed", Message = "Portfolio deployment failed")
         
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
